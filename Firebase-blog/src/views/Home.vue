608ebf86bfe9ced799eeab54e3361c7cc53b240f
Younes <younes-@hotmail.fr>
2021-11-02 13:21:54 +0100

    blog card component

--- Firebase-blog/src/views/Home.vue
+++ Firebase-blog/src/views/Home.vue
@@ -2,14 +2,23 @@
   <div class="home">
     <BlogPost :post="welcomeScreen"/>
     <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+    <div class="blog-card-wrap">
+      <div class="container">
+        <h3>View More Recent Blogs</h3>
+        <div class="blog-cards">
+          <BlogCard :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 import BlogPost from "../components/BlogPost"
+import BlogCard from "../components/BlogCard"
 export default {
   name: "Home",
-  components: { BlogPost },
+  components: { BlogPost, BlogCard },
   data() {
     return {
       welcomeScreen: {
@@ -30,7 +39,24 @@ export default {
           blogCoverPhoto: "designed-for-everyone",
         }
       ],
-    }
-  }
+      sampleBlogCards: [
+        { blogTitle: "Blog card #1", blogCoverPhoto: "stock-1", blogDate: "November 1, 2021"},
+        { blogTitle: "Blog card #2", blogCoverPhoto: "stock-1", blogDate: "November 1, 2021"},
+        { blogTitle: "Blog card #3", blogCoverPhoto: "stock-1", blogDate: "November 1, 2021"},
+        { blogTitle: "Blog card #4", blogCoverPhoto: "stock-1", blogDate: "November 1, 2021"},
+      ],
+    };
+  },
 };
 </script>
+<style lang="scss" scoped>
+
+.blog-card-wrap {
+  h3 {
+    font-weight: 300;
+    font-size: 28px;
+    margin-bottom: 32px;
+  }
+}
+
+</style>
